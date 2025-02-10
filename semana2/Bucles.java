2f2c27c9e6b8a986602596835d95d9a264ada17b
Daniel-LM2608 <kevin.lopezmon@campusucc.edu.co>
2025-02-10 14:52:00 +0000

    Bluces

---
+++ semana2/Bucles.java
@@ -0,0 +1,11 @@
+public class Bucles {
+    public static void main(String[] args) {
+         
+        //for
+
+        for(int i = 1; i <= 5; i++){
+            System.out.println(i + ": Programacion Orientada a Objetos");
+        }
+    }
+    
+}
