3fff91ecda616bc930c3b91aeab5b7960b5e16f3
Daniel-LM2608 <kevin.lopezmon@campusucc.edu.co>
2025-02-10 13:42:21 +0000

    Variable

---
+++ semana2/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+        public static void main ( String [] args ) {
+        //Decalaracion variable entera
+        int edad = 25;
+        //Decalaracion variable real
+        double altura = 1.75;
+        //Decalaracion variable cadena
+        String nombre = " Juan ";
+        //Decalaracion variable boolean
+        boolean estudiante = true ;
+        System . out . println (" Nombre : " + nombre + ", Edad :" + edad + ", Altura :" + altura + ", Estudiante :" + estudiante );
+        }
+        }
