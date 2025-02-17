c40fe239a74b8b2ad0a1a7c008842f225654abd6
Daniel-LM2608 <kevin.lopezmon@campusucc.edu.co>
2025-02-17 14:36:22 +0000

    Arreglos

--- semana3/Arreglos.java
+++ semana3/Arreglos.java
@@ -2,39 +2,36 @@ public class Arreglos {
 
     public static void main(String[] args) {
         /*
-        * primera forma de declarar un arreglo:
-        * 
-        *
-        *
-        *
-        * El numero hace referencia al tamaño del arreglo
-        * tendremos que llenar el arrreglo.
-        */
+         * primera forma de declarar un arreglo:
+         * 
+         *
+         *
+         *
+         * El numero hace referencia al tamaño del arreglo
+         * tendremos que llenar el arrreglo.
+         */
 
-        int[] a = { 8, 3, 10, 22, 1, 9};
+        int[] a = { 8, 3, 10, 22, 1, 9 };
 
         for (int i = 0; i < a.length; i++) {
             System.out.println("a[" + i + "]=" + a[i]);
         }
 
+        // ------------------------
 
+        int tam = 10;
+        int[] b = new int[tam];
 
-           //------------------------
+        // Estructura para llenar el arreglo, con numeros aleatoreos
+        // entre 10 y 30
+        for (int i = 0; i < b.length; i++) {
+            b[i] = (int) (Math.random() * (30 - 10)) + 10;
+        }
 
-           int tam = 10;
-           int[] b = new int[tam];
-   
-           //Estructura para llenar el arreglo, con numeros aleatoreos 
-           //entre 10 y 30
-           for(int i = 0; i < b.length; i++) {
-               b[i] = (int) (Math.random() * (30 - 10)) + 10;
-           }
-   
-           //mostrar los elementos del arreglo
-           for (int i = 0; i < b.length; i++) {
-               System.out.println("b[" + i + "]=" + b[i]);
-           }
-       
-       }
+        // mostrar los elementos del arreglo
+        for (int i = 0; i < b.length; i++) {
+            System.out.println("b[" + i + "]=" + b[i]);
+        }
+
+    }
 }
-     
