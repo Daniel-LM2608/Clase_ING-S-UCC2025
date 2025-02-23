be68d6111e28cc13793570075b092f1b6f53f42a
Daniel-LM2608 <kevin.lopezmon@campusucc.edu.co>
2025-02-23 05:40:06 +0000

    Ejercicio4

--- semana4/Ejercicio4.java
+++ semana4/Ejercicio4.java
@@ -9,5 +9,25 @@ public class Ejercicio4 {
         for (int i = 0; i < b.length; i++) {
             System.out.println("b[" + i + "]=" + b[i]);
         }
+        int tam2 = 10;
+        int[] c = new int[tam2];
+        for (int i = 0; i < c.length; i++) {
+            c[i] = (int) (Math.random() * (30 - 10)) + 10;
+        }
+        for (int i = 0; i < c.length; i++) {
+            System.out.println("c[" + i + "]=" + c[i]);
+        }
+        int edadMayor = b[0];
+        for (int edad : b){
+            if (edad > edadMayor){
+                edadMayor = edad;
+            }
+        }
+        for (int edad : c){
+            if (edad > edadMayor){
+                edadMayor = edad;
+            }
+        }
+        System.out.println("La mayor edad de ambos grupos es:" + edadMayor);
     }
 }
