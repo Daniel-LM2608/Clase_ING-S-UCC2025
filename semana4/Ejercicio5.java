d5fcca50c4669c7d20856fd6ff80070df8d76342
Daniel-LM2608 <kevin.lopezmon@campusucc.edu.co>
2025-03-03 15:02:42 +0000

    Progreso

--- semana4/Ejercicio5.java
+++ semana4/Ejercicio5.java
@@ -3,33 +3,34 @@ import java.util.Scanner;
 public class Ejercicio5 {
 
     public static void main(String[] args) {
-        Scanner leer = new Scanner(System.in);
-        System.out.println("por favor digite la cantidad de viviendas: ");
-        int n = leer.nextInt();
+        try (Scanner leer = new Scanner(System.in)) {
+            System.out.println("por favor digite la cantidad de viviendas: ");
+            int n = leer.nextInt();
 
-        int[] alquileres = new int[n];
+            int[] alquileres = new int[n];
 
-        for (int i = 0; i < alquileres.length; i++) {
-            alquileres[i] = (int) (Math.random() * (1000 + 100)) + 100;
-        }
-        int[] porcentaje = new int[n];
-        for (int i = 0; i < porcentaje.length; i++) {
-            porcentaje[i] = (int) (Math.random() * (100 + 1)) + 1;
-        }
-        int[] ganancia = new int[alquileres.length];
+            for (int i = 0; i < alquileres.length; i++) {
+                alquileres[i] = (int) (Math.random() * (1000 + 100)) + 100;
+            }
+            int[] porcentaje = new int[n];
+            for (int i = 0; i < porcentaje.length; i++) {
+                porcentaje[i] = (int) (Math.random() * (100 + 1)) + 1;
+            }
+            int[] ganancia = new int[alquileres.length];
 
-        for (int i = 0; i < ganancia.length; i++) {
-            ganancia[i] = alquileres[i] * porcentaje[i] / 100;
-        }
+            for (int i = 0; i < ganancia.length; i++) {
+                ganancia[i] = alquileres[i] * porcentaje[i] / 100;
+            }
 
-        for (int i = 0; i < alquileres.length; i++) {
-            System.out.println("alquileres[" + i + "]=" + alquileres[i]);
-        }
-        for (int i = 0; i < porcentaje.length; i++) {
-            System.out.println("porcentajes[" + i + "]=" + porcentaje[i]);
-        }
-        for (int i = 0; i < ganancia.length; i++) {
-            System.out.println("ganacia de la vivienda[" + i + "]=" + ganancia[i]);
+            for (int i = 0; i < alquileres.length; i++) {
+                System.out.println("alquileres[" + i + "]=" + alquileres[i]);
+            }
+            for (int i = 0; i < porcentaje.length; i++) {
+                System.out.println("porcentajes[" + i + "]=" + porcentaje[i]);
+            }
+            for (int i = 0; i < ganancia.length; i++) {
+                System.out.println("ganacia de la vivienda[" + i + "]=" + ganancia[i]);
+            }
         }
     }
 }
---
+++ semana5/ActividadEvaluativa.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ActividadEvaluativa {
+
+    public static void main(String[] args) {
+
+        int tam = 21;
+        int[] puestos = new int[tam];
+
+        for (int i = 0; i < puestos.length; i++) {
+            System.out.println("b[" + i + "]=" + puestos[i]);
+        }
+
+        
+
+    }
+}
+
+        
+        
