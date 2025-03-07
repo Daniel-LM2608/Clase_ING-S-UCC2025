f8bce4d14dd84b753a594883b24b097f4e6da6d1
Daniel-LM2608 <kevin.lopezmon@campusucc.edu.co>
2025-03-07 01:39:21 +0000

    AVANCES

--- semana5/ActividadEvaluativa.java
+++ semana5/ActividadEvaluativa.java
@@ -6,38 +6,43 @@ public class ActividadEvaluativa {
 
         int tam = 21;
         boolean[] ocupado = new boolean[tam]; // para saber si esta libre false o ocupado true
-        String[] placas = new String [tam]; //para guardas las placas moto en los puestos 
+        String[] placas = new String[tam]; // para guardas las placas moto en los puestos
         Scanner scanner = new Scanner(System.in);
-        while (true) {
-            
-        }
 
-        System.out.println("Seleccione un puetso del o al 20");
-        int puesto = scanner.nextInt(); // para que eliga un puesto
-        scanner.nextLine();
-        
-        if(puesto >= 0 && puesto < tam) { // para saber si el puesto exite
-            if(!ocupado[puesto]){ // para ver si el puesto esta libre o ocupado
-                System.out.println("Ingrese la placa");
-                placas[puesto] = scanner.nextLine();// para guardar la placa de la moto
-                ocupado[puesto] = true;
-                System.out.println("Moto registrada en el puesto " + puesto);
 
-            } else {
-                System.out.println("El puesto esta ocupado, eliga otro puesto.");
+        while (true) { // me sirve para hacer un bucle para poder regristrar motos y tambien para salir
+                       // del bucle
+            // para mostrar los puestos disponibles
+            System.out.println("Estado del parqueadero");
+            for (int i = 0; i < tam; i++) {
+                if (ocupado[i]) { // si el puesto esta ocupado
+                    System.out.println("[X]");
+
+                } else { // si el puesto esta libre
+                    System.out.println("[" + i + "]");
+                }
             }
-        }else {
-            System.out.println("Numero de puesto invalido o no existe");
+            System.out.println("Seleccione un puetso del o al 20 o (-0 para salir)");
+            int puesto = scanner.nextInt(); // para que eliga un puesto
+            scanner.nextLine();
+            if (puesto == -0){
+                System.out.println("Saliendo del programa");
+                break;
+            }
+            if (puesto >= 0 && puesto < tam) { // para saber si el puesto exite
+                if (!ocupado[puesto]) { // para ver si el puesto esta libre o ocupado
+                    System.out.println("Ingrese la placa");
+                    placas[puesto] = scanner.nextLine();// para guardar la placa de la moto
+                    ocupado[puesto] = true;
+                    System.out.println("Moto registrada en el puesto " + puesto);
+                } else {
+                    System.out.println("El puesto esta ocupado, eliga otro puesto.");
+                }
+            } else {
+                System.out.println("Numero de puesto invalido o no existe");
+            }
+
         }
-        
-
-
-
-
 
     }
-
 }
-
-        
-        
