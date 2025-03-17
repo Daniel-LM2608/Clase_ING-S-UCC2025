ff74e52f8e3530aef6d1ab79ccbf538bb80d18c7
Daniel-LM2608 <kevin.lopezmon@campusucc.edu.co>
2025-03-17 14:09:23 +0000

    Ejecutar

---
+++ semana6/Ejecutar.java
@@ -0,0 +1,11 @@
+public class Ejecutar {
+
+    public static void main(String[] args) {
+
+    //Instanciar el onjeto (creacion)
+    Estudiante obEst1 = new Estudiante(1, "Daniel", "Moncayo", 19, "Ingenieria");
+    Estudiante obEst2 = new Estudiante(2, "Santiago", "Callejas", 22, "Fisioterapia");
+
+    
+    }
+}
