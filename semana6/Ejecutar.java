440370aa869aeb7c822715ef5069f4784e64c30d
Daniel-LM2608 <kevin.lopezmon@campusucc.edu.co>
2025-03-17 14:26:32 +0000

    Ejecutar

--- semana6/Ejecutar.java
+++ semana6/Ejecutar.java
@@ -2,10 +2,17 @@ public class Ejecutar {
 
     public static void main(String[] args) {
 
-    //Instanciar el onjeto (creacion)
-    Estudiante obEst1 = new Estudiante(1, "Daniel", "Moncayo", 19, "Ingenieria");
-    Estudiante obEst2 = new Estudiante(2, "Santiago", "Callejas", 22, "Fisioterapia");
+        // Instanciar el onjeto (creacion)
+        Estudiante obEst1 = new Estudiante(1, "Daniel", "Moncayo", 19, "Ingenieria");
+        Estudiante obEst2 = new Estudiante(2, "Santiago", "Callejas", 22, "Fisioterapia");
 
-    
+        System.out.println(obEst1); // toString
+        System.out.println(obEst2);
+
+        // Modificar la edad de estudiante
+        obEst1.setEdad(20);
+
+        System.out.println(obEst1);
+
     }
 }
