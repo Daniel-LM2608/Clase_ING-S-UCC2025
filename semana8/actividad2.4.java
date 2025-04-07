73d4d556f78a009fcb44b7756691f8b9cbfe6fae
Daniel-LM2608 <kevin.lopezmon@campusucc.edu.co>
2025-04-07 14:53:44 +0000

    ACT2

--- semana8/actividad2.4.java
+++ semana8/actividad2.4.java
@@ -2,12 +2,18 @@ public class Cabina extends Equipaje {
     // Constantes
     private final static int TIEMPO = 2;
     // Constructores
-   
+   public Cabina(double peso, double tamanio, double precioFinal) {
+    //super para heredar de equipaje sin tener que repetirlo y reutilizar codigo
+    super(peso, tamanio, 1000);//llamar constructor de equipaje con precio de 1000
+   }
     // Metodos
     public double calcularPrecio(){
+        double precioFinal = getPrecioBase() + INCREMENTO_CABINA;
     // Calculos
     return precioFinal;
     }
+
+    
     // getters/setters de ser necesarios
    }
    // Fin de la solución
