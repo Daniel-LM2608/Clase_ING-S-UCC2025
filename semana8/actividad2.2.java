2579fd0f865217f71ff7a3da9aab9489bec02eb4
Daniel-LM2608 <kevin.lopezmon@campusucc.edu.co>
2025-04-07 14:09:06 +0000

    act2

--- semana8/actividad2.2.java
+++ semana8/actividad2.2.java
@@ -8,10 +8,53 @@ public class Equipaje {
     private double tamanio;
     private double precioBase;
     // Constructores
+    //por defecto
+    public Equipaje() {
+        this.peso = PESO;
+        this.tamanio = TAMANIO;
+        this.precioBase = PRECIO_BASE;
+    }
+
+    //con parametros 
+    public Equipaje(double peso,double tamanio, double precioBase) {
+        this.peso = peso;
+        this.tamanio = tamanio;
+        this.precioBase = precioBase;
+    }
+
    
     // Metodos
     public double calcularPrecio(){
     return 0.0;
     }
+
+
     // getters/setters de ser necesarios
-   }
+    //getters
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public double getTamamio() {
+        return tamanio;
+    }
+
+    public double getPrecioBase() {
+        return precioBase;
+    }
+
+    //setters
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public void setTamanio(double tamanio) {
+        this.tamanio = tamanio;
+    }
+
+    public void setPrecioBase(double precioBase) {
+        this.precioBase = precioBase;
+    }
+
