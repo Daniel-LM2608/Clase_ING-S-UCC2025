1bec5c9c603d8ef52bf5479cf421b13ca04278a9
Daniel-LM2608 <kevin.lopezmon@campusucc.edu.co>
2025-04-21 06:48:50 +0000

    act2

--- semana8/Bodega.java
+++ semana8/Bodega.java
@@ -1,21 +1,21 @@
 public class Bodega extends Equipaje {
-    private static final double CAPACIDAD = 8.0; // define variable constante 
+    private static final double CAPACIDAD = 8.0; // define variable constante
 
     public Bodega(double peso, double tamanio) { // constructor crea objeto tipo bodega
-        super(peso, tamanio); //lama al constructor de la clase padre, que en este caso es la clase Equipaje.
+        super(peso, tamanio); // lama al constructor de la clase padre, que en este caso es la clase Equipaje.
     }
 
     public Bodega(double precioBase) {
-        super(precioBase); //lama al constructor de la clase padre, que resive solo el precio base
+        super(precioBase); // lama al constructor de la clase padre, que resive solo el precio base
     }
-    
 
     public Bodega() {
-        super(); // el super usa los valores por defecto en equipaje 
+        super(); // el super usa los valores por defecto en equipaje
     }
 
-    @Override // Verifica y marca un método como sobrescrito 
-    //Evita errores y mejora la claridad del código no es obligatorio pero recomendable
+    @Override // Verifica y marca un método como sobrescrito
+    // Evita errores y mejora la claridad del código no es obligatorio pero
+    // recomendable
     public double calcularPrecio() {
         return getPrecioBase() + (getPeso() * getTamanio() * CAPACIDAD);
     }
--- semana8/PrecioTotal.java
+++ semana8/PrecioTotal.java
@@ -13,27 +13,30 @@ public class PrecioTotal {
         totalBodega = 0;
         totalCabina = 0;
 
-        for (Equipaje e : equipaje) {//e es un objeto del arreglo  que me sirve para no tener que mensionar el objeto como tal 
-            // se usa for each Es una forma simple de recorrer todos los elementos de un arreglo o colección, sin usar índices.
+        for (Equipaje e : equipaje) {// e es un objeto del arreglo que me sirve para no tener que mensionar el objeto
+                                     // como tal
+            // se usa for each Es una forma simple de recorrer todos los elementos de un
+            // arreglo o colección, sin usar índices.
             double precio = e.calcularPrecio();
             totalPrecios += precio;
 
             if (e instanceof Bodega) {
-                totalBodega += precio;//instanceof es una forma común de trabajar con herencia 
-                //cuando necesitas saber qué subclase está siendo usada.
-                //Se usa cuando tienes una superclase como Equipaje y quieres saber si un objeto 
-                //es realmente una subclase, como Cabina o Bodega.
+                totalBodega += precio;// instanceof es una forma común de trabajar con herencia
+                // cuando necesitas saber qué subclase está siendo usada.
+                // Se usa cuando tienes una superclase como Equipaje y quieres saber si un
+                // objeto
+                // es realmente una subclase, como Cabina o Bodega.
             } else if (e instanceof Cabina) {
                 totalCabina += precio;
-            }//Recorre uno por uno todos los objetos que hay en el arreglo equipaje.
+            } // Recorre uno por uno todos los objetos que hay en el arreglo equipaje.
 
-            //A cada objeto le llama el método calcularPrecio() para saber cuánto cuesta.
-            
-            //Va sumando el precio total general (totalPrecios).
-            
-            //Si el objeto es una Bodega, suma su precio al total de bodegas (totalBodega).
-            
-            //Si el objeto es una Cabina, suma su precio al total de cabinas (totalCabina).
+            // A cada objeto le llama el método calcularPrecio() para saber cuánto cuesta.
+
+            // Va sumando el precio total general (totalPrecios).
+
+            // Si el objeto es una Bodega, suma su precio al total de bodegas (totalBodega).
+
+            // Si el objeto es una Cabina, suma su precio al total de cabinas (totalCabina).
         }
     }
 
@@ -45,8 +48,8 @@ public class PrecioTotal {
     }
 }
 
-    // Métodos
-    //e es un objeto del arreglo  que me sirve para no tener que mensionar el objeto como tal 
-    // se usa for each Es una forma simple de recorrer todos los elementos de un arreglo o colección, sin usar índices.
-
-    
+// Métodos
+// e es un objeto del arreglo que me sirve para no tener que mensionar el objeto
+// como tal
+// se usa for each Es una forma simple de recorrer todos los elementos de un
+// arreglo o colección, sin usar índices.
