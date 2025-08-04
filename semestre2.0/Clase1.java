8c869f44b6bcc1eef1cf7e529bd9b69a121fe70c
Daniel-LM2608 <kevin.lopezmon@campusucc.edu.co>
2025-08-04 14:03:24 +0000

    Clase1

---
+++ semestre2.0/Clase1.java
@@ -0,0 +1,19 @@
+public class Clase1 {
+    public static void main(String[] args) {
+        
+        System.out.println("Estructura de datos");
+
+        //Arreglos o arrays o vectores
+        
+        //Declaracion del arreglo. forma1
+        int[] numeros;
+        numeros = new int[5];
+
+        //Declaracion del arreglo. forma2
+        //int[] numeros = new int[5];
+
+        //Declaracion e inicializacion 
+        int[] numeros = {10, 20, 30, 40, 50};
+    }
+    
+}
