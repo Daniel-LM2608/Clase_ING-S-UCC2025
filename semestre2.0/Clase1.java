b8314ebafccdf3968684485c74fcea5f4fb00fa3
Daniel-LM2608 <kevin.lopezmon@campusucc.edu.co>
2025-08-04 14:27:28 +0000

    operaArre

--- semestre2.0/Clase1.java
+++ semestre2.0/Clase1.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Clase1 {
     public static void main(String[] args) {
         
@@ -6,14 +8,20 @@ public class Clase1 {
         //Arreglos o arrays o vectores
         
         //Declaracion del arreglo. forma1
-        int[] numeros;
-        numeros = new int[5];
+        //int[] numeros;
+        //numeros = new int[5];
 
         //Declaracion del arreglo. forma2
         //int[] numeros = new int[5];
 
         //Declaracion e inicializacion 
-        int[] numeros = {10, 20, 30, 40, 50};
+        //int[] numeros = {10, 20, 30, 40, 50};
+
+        OperacionesArreglos objArreglos = new OperacionesArreglos();
+
+        int[] numeros = objArreglos.llenarArregloEnteros(5);
+
+        System.out.println(Arrays.toString(numeros));
     }
     
 }
---
+++ semestre2.0/OperacionesArreglos.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class OperacionesArreglos {
+
+    //metodo para llenar un arreglo de enteros
+    public int[] llenarArregloEnteros(int tam){
+        
+        Random rand = new Random();
+
+        int[] numeros = new int[tam];
+
+        for(int i = 0; i < numeros.length; i++){
+            numeros[i] = rand.nextInt(100 - 1) + 1; //numeros del 1 al 100
+
+        }
+
+        return numeros;
+
+
+    }
+    
+}
