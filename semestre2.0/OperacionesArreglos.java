c37e73ec4488cb73fb3d8fb16e4c548550aa0101
Daniel-LM2608 <kevin.lopezmon@campusucc.edu.co>
2025-08-10 05:08:41 +0000

    falta numeros repetidos

--- semestre2.0/OperacionesArreglos.java
+++ semestre2.0/OperacionesArreglos.java
@@ -2,80 +2,55 @@ import java.util.Random;
 
 public class OperacionesArreglos {
 
-    //metodo para llenar un arreglo de enteros
-    public int[] llenarArregloEnteros(int tam){
-        
+    // metodo para llenar un arreglo de enteros
+    public int[] llenarArregloEnteros(int tam) {
+
         Random rand = new Random();
 
         int[] numeros = new int[tam];
 
-        for(int i = 0; i < numeros.length; i++){
-            numeros[i] = rand.nextInt(100 - 1) + 1; //numeros del 1 al 100
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = rand.nextInt(100 - 1) + 1; // numeros del 1 al 100
 
         }
 
-         //Sumar los elementos de un arreglo de enteros
+        // Sumar los elementos de un arreglo de enteros
         int suma = 0;
         for (int i = 0; i < numeros.length; i++) {
             suma = suma + numeros[i];
         }
 
         System.out.println("La suma de los elementos del arreglo es:" + suma);
-        
-        
-        //Obtener el promedio de un arreglo de enteros
+
+        // Obtener el promedio de un arreglo de enteros
         double promedio = 0;
         for (int i = 0; i < numeros.length; i++) {
             promedio = promedio + (double) numeros[i] / 5; // convertir el a decimal antes de dividr
-            
+
         }
 
         System.out.println("El promedio de los elementos del arreglo es:" + promedio);
 
-
-        //Obtener los numeros repetidos de un arreglo de enteros
-
-
-
-
-
-
-
-
-
-
-
-
-        //Obtener el mayor y el menor de un arreglo de enteros
+        // Obtener el mayor y el menor de un arreglo de enteros
         int mayor = numeros[0], menor = numeros[0];
-        
-        for(int num : numeros) {
+
+        for (int num : numeros) {
             if (num > mayor) {
                 mayor = num;
             }
             if (num < menor) {
                 menor = num;
             }
-        
+
         }
-        
-        System.out.println(" el numero mayor es:" + mayor );
-        System.out.println(" el numero menor es:" + menor );
 
-
+        System.out.println(" el numero mayor es:" + mayor);
+        System.out.println(" el numero menor es:" + menor);
 
         return numeros;
 
-    
-
-
-    
-
-    //Obtener los numeros repetidos de un arreglo de enteros
-
-    
-
+        // Obtener los numeros repetidos de un arreglo de enteros
 
     }
-    
+
 }
