2bc09423006e027b7da4d644dea161cad510bbac
Daniel-LM2608 <kevin.lopezmon@campusucc.edu.co>
2025-08-10 03:34:47 +0000

    resolver suma

--- semestre2.0/OperacionesArreglos.java
+++ semestre2.0/OperacionesArreglos.java
@@ -14,9 +14,15 @@ public class OperacionesArreglos {
 
         }
 
-        return numeros;
-
          //Sumar los elementos de un arreglo de enteros
+        int suma = 0;
+        for (int i = 0; i < numeros.length; i++) {
+            suma = suma + numeros[i];
+        }
+
+        System.out.println("La suma de los elementos del arreglo es:" + suma);
+
+        return numeros;
 
     //Obtener el promedio de un arreglo de enteros
 
