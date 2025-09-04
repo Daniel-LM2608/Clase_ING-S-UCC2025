8ace97b954846aaface81dfacb73830d176e3aab
Daniel-LM2608 <kevin.lopezmon@campusucc.edu.co>
2025-09-04 20:29:57 +0000

    clase paquete con gets y sus atributos tambien el tostring

--- MiniProyecto/paquete.java
+++ MiniProyecto/paquete.java
@@ -5,5 +5,30 @@ public package MiniProyecto;
 
 class paquete {
 
+     private String destinatario;
+     private String tamano;
+     private String fechaIngreso;
+
+     public paquete (String destinatario, String tamano){
+        this.destinatario = destinatario;
+        this.tamano = tamano;
+        this.fechaIngreso = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm"));
+    }
      
+    public String getDestinatario(){
+        return destinatario;
+    }
+    public String getTamano(){
+        return tamano;
+    }
+    public String getFechaIngreso(){
+        return fechaIngreso;
+    }
+      public String toString() {
+        return "paquete { Destinatario:" + destinatario + 
+               " Tamaño:" + tamano + 
+               " Fecha De Ingreso:" + fechaIngreso + "}";
+    }   
+    
+
 }
