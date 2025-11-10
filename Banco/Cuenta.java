b90637dd07aac5cf3defe47c621c15966eefdb1f
Daniel-LM2608 <kevin.lopezmon@campusucc.edu.co>
2025-11-10 15:24:54 +0000

    metodo consultar saldo y apertura(para abrir cuenta)

--- Banco/Cuenta.java
+++ Banco/Cuenta.java
@@ -1,31 +1,45 @@
 public class Cuenta {
     private double apertura;
-    protected double montoActual;
+    protected double montoActual = 0;
     protected double cantidad;
 
-    public Cuenta(double apertura,double montoActual,double cantidad){ 
+    public Cuenta(double apertura, double montoActual, double cantidad) {
         this.apertura = apertura;
         this.montoActual = montoActual;
         this.cantidad = cantidad;
     }
+
     public double getApertura() {
         return apertura;
     }
+
     public void setApertura(double apertura) {
         this.apertura = apertura;
     }
+
     public double getMontoActual() {
         return montoActual;
     }
+
     public void setMontoActual(double montoActual) {
         this.montoActual = montoActual;
     }
+
     public double getCantidad() {
         return cantidad;
     }
+
     public void setCantidad(double cantidad) {
         this.cantidad = cantidad;
     }
 
-    
+    public void apertura(double apertura) {
+        this.apertura = apertura;
+        this.montoActual += apertura;
+    }
+
+    public double consultarSaldo() {
+        return montoActual;
+    }
+
 }
